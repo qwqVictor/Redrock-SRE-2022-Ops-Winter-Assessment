77ef39c3bd5ad3e0fc98e5ab4590341cb30d7dc6
Smart-SangGe <2251250136@qq.com>
2022-02-10 19:19:06 +0800

    complete shellcheck

--- Q3/nginxyijian.sh
+++ Q3/nginxyijian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if user is root
-if [ $(id -u) != "0" ]; then
+if [ "$(id -u)" != "0" ]; then
     echo "Error: You must be root to run this script."
     exit 1
 fi
@@ -11,13 +11,13 @@ echo "Preparing environment"
 apt install build-essential wget curl automake pcre pcre-devel zlib zlib-devel open openssl-devel -y
 useradd -s /sbin/nologin nginx
 echo "Environment is done"
-flag=`nginx -v`
-latest=`curl https://nginx.org/en/download.html|grep 'nginx-.*gz'|awk -F '-' '{print $2}'|sed 's/.tar.gz">nginx//g'`
-current=`nginx -v 2>&1 |sed 's/nginx version: nginx\///g'`
-if [-z $flag];then
+flag=$(nginx -v)
+latest=$(curl https://nginx.org/en/download.html|grep 'nginx-.*gz'|awk -F '-' '{print $2}'|sed 's/.tar.gz">nginx//g')
+current=$(nginx -v 2>&1 |sed 's/nginx version: nginx\///g')
+if [ -z "$flag" ];then
 	echo "You didn't install nginx"
 else
-	if [$current == $latest]; then
+	if [ "$current" == "$latest" ]; then
         	echo Your version is latest
 		else
 			echo "The lateset version is $latest, your version is $current"
@@ -28,22 +28,22 @@ echo "Input install version: [default=$latest]"
 while true
 do
 	read -r version < /dev/tty
-	flag=`curl https://nginx.org/download/|grep $version.tar`
+	flag=$(curl https://nginx.org/download/|grep "$version".tar)
 	if [ -z "$flag" ]; then
     	echo "Wrong version, input again"
-	elif [-z $version ];then
+	elif [ -z "$version" ];then
 		version=$latest
 	fi
 done
 
-wget http://nginx.org/download/nginx-$version.tar.gz
-tar -zxvf nginx-$version.tar.gz
-cd nginx-$version
+wget http://nginx.org/download/nginx-"$version".tar.gz
+tar -zxvf nginx-"$version".tar.gz
+cd nginx-"$version" || exit
 
 inst(){
 	echo "Input install parameter"
-	ipath=`nginx -V 2>&1|grep configure|sed "s/configure arguments://g"|tr ' ' '\n'|grep prefix=|awk -F '=' '{print $2}'`
-	if [ -z $ipath ]; then
+	ipath=$(nginx -V 2>&1|grep configure|sed "s/configure arguments://g"|tr ' ' '\n'|grep prefix=|awk -F '=' '{print $2}')
+	if [ -z "$ipath" ]; then
 		echo "The path to install [default /usr/local/nginx]"
 		read -r ipath < /dev/tty
 		if [ -z "$ipath" ]; then
@@ -52,15 +52,13 @@ inst(){
 	else
 		echo "The path to install [default $ipath]"
 		read -r ipatha < /dev/tty
-		if [ -z "$ipatha" ]; then
-	   	continue
-		else
+		if [ -n "$ipatha" ]; then
 			ipath=$ipatha
 		fi
 	fi
 
-	epath=`nginx -V 2>&1|grep configure|sed "s/configure arguments://g"|tr ' ' '\n'|grep error.log|awk -F '=' '{print $2}'`
-	if [ -z $epath ]; then
+	epath=$(nginx -V 2>&1|grep configure|sed "s/configure arguments://g"|tr ' ' '\n'|grep error.log|awk -F '=' '{print $2}')
+	if [ -z "$epath" ]; then
     		echo "The path of error log [default /var/log/nginx/error.log]"
 		read -r epath < /dev/tty
 		if [ -z "$epath" ]; then
@@ -69,15 +67,13 @@ inst(){
 	else
 		echo "The path to install [default $epath]"
 		read -r epatha < /dev/tty
-			if [ -z "$epatha" ]; then
-				continue
-			else
+			if [ -n "$epatha" ]; then
 				epath=$epatha
 			fi
 	fi
 
-	apath=`nginx -V 2>&1|grep configure|sed "s/configure arguments://g"|tr ' ' '\n'|grep http.log|awk -F '=' '{print $2}'`
-	if [ -z $apath ]; then
+	apath=$(nginx -V 2>&1|grep configure|sed "s/configure arguments://g"|tr ' ' '\n'|grep http.log|awk -F '=' '{print $2}')
+	if [ -z "$apath" ]; then
 		echo "The path of assess log [default /var/log/nginx/assess.log]"
 		read -r apath < /dev/tty
 		if [ -z "$apath" ]; then
@@ -86,21 +82,19 @@ inst(){
 	else
 		echo "The path to install [default $apath]"
 		read -r apatha < /dev/tty
-		if [ -z "$apatha" ]; then
-   			continue
-		else
+		if [ -n "$apatha" ]; then
 			apath=$apatha
 		fi
 	fi
 
 	echo "Installing"
 
-	./configure --prefix=$ipath \
+	./configure --prefix="$ipath" \
 	--conf-path=/etc/nginx/nginx.conf \
 	--user=nginx \
 	--group=nginx \
-	--error-log-path=$epath \
-	--http-log-path=$apath \
+	--error-log-path="$epath" \
+	--http-log-path="$apath" \
 	--pid-path=/var/run/nginx/nginx.pid \
 	--lock-path=/var/lock/nginx.lock \
 	--with-http_ssl_module \
@@ -115,15 +109,15 @@ inst(){
 	make && make install
 	echo "Finish"
 }
-argu=`nginx -V 2>&1|grep configure|sed "s/configure arguments://g"`
-if [ -n $argu ]; then
+argu=$(nginx -V 2>&1|grep configure|sed "s/configure arguments://g")
+if [ -n "$argu" ]; then
 	echo "Find exist parameters, use it?[Y/n]"
 	read -r use < /dev/tty
-	if [ $use == N] || [ $use == n ]; then
+	if [ "$use" == "N" ] || [ "$use" == "n" ]; then
 		inst	
 	else
 		echo "Installing"
-		./configure $argu
+		./configure "$argu"
 		make&&make install
 		echo "Finish"
 	fi
